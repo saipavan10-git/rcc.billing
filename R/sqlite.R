2cf7ef7aeafa157b170b59120f70990c5cb3de56
Philip Chase <pbc@ufl.edu>
2022-03-30 12:36:43 -0400

    Fix convert_schema_to_sqlite*()

--- R/sqlite.R
+++ R/sqlite.R
@@ -1,7 +1,10 @@
-#' Converts a mysql schema to a sqlite schema for the provided table.
-#' A corresponding schema file with the same name is required in /schema.
+#' Locates a MySQL schema file for table_name, converts it to a sqlite schema
+#' and returns that schema.
 #'
 #' @param table_name, the name of the table to convert
+#'
+#' @returns sqlite schema for table_name
+#' @importFrom magrittr "%>%"
 #'
 #' @examples
 #' \dontrun{
@@ -11,48 +14,37 @@
 convert_schema_to_sqlite <- function(table_name) {
   schema_file_name <- paste0(table_name, ".sql")
   lite_schema_file_name <- paste0(table_name, "_lite.sql")
-  pl_to_sqlite <- system.file("to_sqlite.pl", package = "rcc.billing")
+  pl_to_sqlite <- system.file("", "to_sqlite.pl", package = "rcc.billing")
 
   # read original
-  original_schema <- readr::read_file(file = system.file("schema", schema_file_name, package = "rcc.billing"))
-
-  # create temp file
-  temp_file <- tempfile(fileext = "sql")
-
-  # write to temp file
-  readr::write_file(x = original_schema, file = temp_file)
+  original_schema_file = system.file("schema", schema_file_name, package = "rcc.billing")
 
   # convert to sqlite
-  cmd <- paste("cat", temp_file, "|", "perl", pl_to_sqlite, ">", paste0("./schema/", lite_schema_file_name))
+  cmd <- paste("cat", original_schema_file, "|", "perl", pl_to_sqlite)
 
-  system(cmd)
+  result <- system(cmd, intern = TRUE) %>% paste(collapse = "")
 }
 
-#' Creates a table in sqlite for the provided table. 
+#' Creates a table in sqlite for the provided table.
 #' A corresponding data set and sqlite schema are required in /data and /schema respectively.
 #' Use \code{\link{convert_schema_to_sqlite}} to generate a sqlite schema from a mysql schema.
 #'
-#' @param table_name, the name of the table to create in sqlite. A _test_data file in /data 
+#' @param table_name, the name of the table to create in sqlite. A _test_data file in /data
 #'
 #' @examples
 #' \dontrun{
 #' write_to_sqlite(table_name = "service_type")
 #' }
 #' @export
-write_to_sqlite <- function(table_name) {
-  schema_file_name <- paste0(table_name, "_lite.sql")
-
+write_to_sqlite <- function(table_name, sqlite_schema) {
   # get test data
   test_data <- get0(paste0(table_name, "_test_data"))
-
-  # read sqlite schema
-  lite_schema <- readr::read_file(system.file("schema", schema_file_name, package = "rcc.billing"))
 
   # connect to sqlite db
   conn <- DBI::dbConnect(RSQLite::SQLite(), dbname = ":memory:")
 
   # create table
-  result <- DBI::dbSendQuery(conn, lite_schema)
+  result <- DBI::dbSendQuery(conn, sqlite_schema)
   # close result set to avoid warning
   DBI::dbClearResult(result)
 
--- examples/service_type.R
+++ examples/service_type.R
@@ -2,5 +2,6 @@ library(rcc.billing)
 
 table_name <- "service_type"
 
-convert_schema_to_sqlite(table_name =  table_name)
-write_to_sqlite(table_name = table_name)
+sqlite_schema <- convert_schema_to_sqlite(table_name =  table_name)
+write_to_sqlite(table_name = table_name,
+                sqlite_schema = sqlite_schema)
