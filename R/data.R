149b7d2819ed39ee56f81c777763ae14b8eb6798
Michael Bentz <mbentz@ufl.edu>
2022-03-31 10:33:42 -0400

    Document test dataset

---
+++ R/data.R
@@ -0,0 +1,17 @@
+#' Sample data for service_type table
+#'
+#' @format
+#' tribble(
+#'   ~service_type_code,
+#'   ~service_type,
+#'   ~cost,
+#'   ~billing_frequency,
+#'   1, "redcap_project_prod", 100, 12,
+#'   2, "redcap_project_phone", 1000, 0,
+#'   3, "redcap_table_account_prod", 35, 6,
+#'   4, "redcap_table_account_phone", 35, 6,
+#'   5, "redcap_mobile", 1000, 0,
+#'   6, "redcap_consulting", 100, 0
+#' )
+#' @source \url{https://github.com/ctsit/rcc.billing/issues/1}
+"service_type_test_data"
