a11094544de251fe94d2c754dccf241fe62a1aed
Michael Bentz <mbentz@ufl.edu>
2022-04-04 10:43:52 -0400

    Add example script and test

---
+++ examples/service_instance.R
@@ -0,0 +1,15 @@
+library(DBI)
+library(rcc.billing)
+
+table_name <- "service_instance"
+conn <- DBI::dbConnect(RSQLite::SQLite(), dbname = ":memory")
+
+sqlite_schema <- convert_schema_to_sqlite(table_name = table_name)
+create_table(
+    conn = conn,
+    schema = sqlite_schema
+)
+populate_table(
+    conn = conn,
+    table_name = table_name
+)
