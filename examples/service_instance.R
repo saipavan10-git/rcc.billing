d03757dd265fd2ebd2e6e393f7a898986867a3b0
Philip Chase <pbc@ufl.edu>
2022-04-06 11:57:59 -0400

    Fix typo in service_instance.R

--- examples/service_instance.R
+++ examples/service_instance.R
@@ -2,7 +2,7 @@ library(DBI)
 library(rcc.billing)
 
 table_name <- "service_instance"
-conn <- DBI::dbConnect(RSQLite::SQLite(), dbname = ":memory")
+conn <- DBI::dbConnect(RSQLite::SQLite(), dbname = ":memory:")
 
 sqlite_schema <- convert_schema_to_sqlite(table_name = table_name)
 create_table(
