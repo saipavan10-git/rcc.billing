f47f33489abaf0027a54bf2c51049f4559e6cf13
Michael Bentz <mbentz@ufl.edu>
2022-04-12 11:42:13 -0400

    Create schema for MySQL table `invoice_line_item_communications`

---
+++ inst/schema/invoice_line_item_communications.sql
@@ -0,0 +1,56 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+CREATE TABLE `invoice_line_item_communications` (
+  `id` double NOT NULL,
+  `service_identifier` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `service_type_code` double DEFAULT NULL,
+  `service_instance_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `ctsi_study_id` double DEFAULT NULL,
+  `name_of_service` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `other_system_invoicing_comments` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `price_of_service` double DEFAULT NULL,
+  `qty_provided` double DEFAULT NULL,
+  `amount_due` double DEFAULT NULL,
+  `fiscal_year` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `month_invoiced` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `pi_last_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `pi_first_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `pi_email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `gatorlink` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `crc_number` double DEFAULT NULL,
+  `ids_number` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `ocr_number` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `invoice_number` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `je_number` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `je_posting_date` datetime DEFAULT NULL,
+  `reason` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `status` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `updated` datetime DEFAULT NULL,
+  `sender` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `recipient` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `date_sent` datetime DEFAULT NULL,
+  `date_received` datetime DEFAULT NULL,
+  `script_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+ALTER TABLE `invoice_line_item_communications`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `service_identifier` (`service_identifier`),
+  ADD KEY `service_type_code` (`service_type_code`),
+  ADD KEY `service_instance_id` (`service_instance_id`),
+  ADD KEY `ctsi_study_id` (`ctsi_study_id`),
+  ADD KEY `fiscal_year` (`fiscal_year`),
+  ADD KEY `month_invoiced` (`month_invoiced`),
+  ADD KEY `gatorlink` (`gatorlink`),
+  ADD KEY `pi_email` (`pi_email`),
+  ADD KEY `pi_last_name` (`pi_last_name`),
+  ADD KEY `pi_first_name` (`pi_first_name`),
+  ADD KEY `status` (`status`),
+  ADD KEY `created` (`created`),
+  ADD KEY `updated` (`updated`),
+  ADD KEY `reason` (`reason`);
+  ADD KEY `sender` (`sender`);
+  ADD KEY `recipient` (`recipient`);
