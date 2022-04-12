79b18109de571203fcd58c971a7562e2cde850df
Michael Bentz <mbentz@ufl.edu>
2022-04-12 13:27:12 -0400

    Add ctsit_staff schema, example and tests

---
+++ inst/schema/ctsit_staff.sql
@@ -0,0 +1,5 @@
+
+CREATE TABLE `ctsit_staff` (
+  `redcap_username` varchar(255) DEFAULT NULL,
+  `employment_intervals` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
---
+++ tests/testthat/test-ctsit_staff.R
@@ -0,0 +1,18 @@
+test_that("service_type sqlite schema is created and correct test data is returned", {
+  table_name <- "ctsit_staff"
+  test_data <- get0(table_name)
+  conn <- DBI::dbConnect(RSQLite::SQLite(), dbname = ":memory:")
+
+  sqlite_schema <- convert_schema_to_sqlite(table_name = table_name)
+  create_table(
+    conn = conn,
+    schema = sqlite_schema
+  )
+  results <- populate_table(
+    conn = conn,
+    table_name = table_name
+  )
+
+  DBI::dbDisconnect(conn)
+  expect_identical(test_data, dplyr::as_tibble(results))
+})
