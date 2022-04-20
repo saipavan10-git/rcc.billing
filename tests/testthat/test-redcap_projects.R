ed29cba896420937856fc24c960ada4c3fe23747
Philip Chase <pbc@ufl.edu>
2022-04-20 07:27:37 -0400

    Update test data and tests for redcap_projects table

--- tests/testthat/test-redcap_projects.R
+++ tests/testthat/test-redcap_projects.R
@@ -12,20 +12,20 @@ test_that("service_type sqlite schema is created and correct test data is return
         conn = conn,
         table_name = table_name
     ) %>%
-        dplyr::mutate(dplyr::across(c(creation_time, production_time, inactive_time, completed_time, date_deleted, last_logged_event, datamart_cron_end_date, twilio_request_inspector_checked), ~ as.POSIXct(., origin = "1970-01-01 00:00.00 UTC", tz = "UTC"))) %>%
+        dplyr::mutate(dplyr::across(c(creation_time,
+                                      production_time,
+                                      inactive_time,
+                                      completed_time,
+                                      date_deleted,
+                                      last_logged_event,
+                                      datamart_cron_end_date,
+                                      twilio_request_inspector_checked),
+                                    ~ as.POSIXct(., origin = "1970-01-01 00:00.00 UTC", tz = "UTC"))) %>%
         dplyr::mutate(
             project_id = as.double(project_id),
-            # what is the proper way of handling this?
-            # `actual$twilio_from_number` is an S3 object of class <integer64>, double
-            #`expected$twilio_from_number` is integer (NA, NA, NA, NA, NA)
-            # twilio_from_number = as.data.frame.vector(twilio_from_number)
-            # twilio_from_number = as.data.frame.integer(as.double(twilio_from_number))
-        ) %>%
-        dplyr::mutate(
             protected_email_mode_custom_trigger = as.null.default(protected_email_mode_custom_trigger)
         )
 
     DBI::dbDisconnect(conn)
-    expect_equal(1,1)
-    #expect_identical(test_data, dplyr::as_tibble(results))
+    expect_equal(dplyr::as_tibble(results), test_data)
 })
