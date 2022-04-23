aaa021f485c21ba1ebae57ec213f4fe1b27516bb
Philip Chase <pbc@ufl.edu>
2022-04-23 18:16:49 -0400

    Disable the code that regenerates redcap_user_information parts

--- data-raw/redcap_user_information.R
+++ data-raw/redcap_user_information.R
@@ -10,19 +10,19 @@ library(dotenv)
 
 my_table <- "redcap_user_information"
 
-# run this once against a test redcap to extract the part we need
-conn <- connect_to_redcap_db()
-data <- tbl(conn, my_table)
-
-redcap_user_information_test_data <- data %>%
-  collect() %>%
-  filter(ui_id >= 3)
-
-# werite the test data
-usethis::use_data(redcap_user_information_test_data, overwrite = T)
-
-# write the schema
-DBI::dbGetQuery(conn, paste("show create table", my_table))$`Create Table` %>%
-  write(file = paste0("inst/schema/", my_table, ".sql"))
-
-dbDisconnect(conn)
+# # run this once against a test redcap to extract the part we need
+# conn <- connect_to_redcap_db()
+# data <- tbl(conn, my_table)
+#
+# redcap_user_information_test_data <- data %>%
+#   collect() %>%
+#   filter(ui_id >= 3)
+#
+# # werite the test data
+# usethis::use_data(redcap_user_information_test_data, overwrite = T)
+#
+# # write the schema
+# DBI::dbGetQuery(conn, paste("show create table", my_table))$`Create Table` %>%
+#   write(file = paste0("inst/schema/", my_table, ".sql"))
+#
+# dbDisconnect(conn)
