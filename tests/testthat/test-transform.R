2afd76f3b7c17f5d9067b4ea9f8869ee84d8e97c
Kyle Chesney <kyle.2493@gmail.com>
2022-05-31 10:48:09 -0700

    Add draft_communication_record_from_line_item function and test

--- tests/testthat/test-transform.R
+++ tests/testthat/test-transform.R
@@ -21,3 +21,28 @@ test_that("transform_invoice_line_items_for_csbt correctly converts all column n
 
   expect_false( any(transformed_colnames %in% csbt_column_names$ctsit) )
 })
+
+
+test_that("draft_communication_record_from_line_item correctly adds requisite columns", {
+  csbt_line_items <- invoice_line_item_test_data %>%
+    transform_invoice_line_items_for_csbt()
+
+  communication_records <- csbt_line_items %>%
+    draft_communication_record_from_line_item()
+
+  added_columns <- c(
+    "updated",
+    "sender",
+    "recipient",
+    "date_sent",
+    "date_received",
+    "script_name"
+  )
+
+  redcapcustodian::set_script_name("test")
+  redcapcustodian::set_script_run_time()
+
+  expect_true(
+    all( added_columns %in% colnames(communication_records) )
+  )
+})
