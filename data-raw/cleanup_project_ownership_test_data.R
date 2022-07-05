4f5d02f5042e15394cd41efad3efc6ecfbaf593a
Philip Chase <pbc@ufl.edu>
2022-07-05 18:34:03 -0400

    Add cleanup_project_ownership_table.R
    Add cleanup_project_ownership_test_data
    Add get_creators()
    Add get_last_project_user()
    Add get_privileged_user()
    Add get_project_pis()
    Add get_projects_needing_new_owners()
    Add get_projects_without_owners()
    Ignore ENV files that might have secrets.

---
+++ data-raw/cleanup_project_ownership_test_data.R
@@ -0,0 +1,54 @@
+## Create cleanup_project_ownership_test_data
+
+library(redcapcustodian)
+library(rcc.billing)
+library(tidyverse)
+library(lubridate)
+library(DBI)
+library(RMariaDB)
+library(dotenv)
+
+# run this once against a test redcap to extract the part we need
+conn <- connect_to_redcap_db()
+
+redcap_user_information <- tbl(conn, "redcap_user_information") %>%
+  collect() %>%
+  filter(ui_id >= 2)
+
+redcap_projects <- tbl(conn, "redcap_projects") %>%
+  collect()
+
+redcap_user_rights <- tbl(conn, "redcap_user_rights") %>%
+  collect()
+
+redcap_user_roles <- tbl(conn, "redcap_user_roles") %>%
+  collect()
+
+redcap_entity_project_ownership <- tbl(conn, "redcap_entity_project_ownership") %>%
+  collect()
+
+redcap_project_last_users <- tribble(
+  ~project_id, ~last_user,
+  123, "dummy"
+) %>% filter(F)
+
+for (pid in redcap_projects$project_id) {
+  redcap_project_last_users <- redcap_project_last_users %>%
+    add_row(
+      project_id = pid,
+      last_user = get_last_project_user(conn, pid)
+    )
+}
+
+dbDisconnect(conn)
+
+cleanup_project_ownership_test_data <- list(
+  redcap_user_information = redcap_user_information,
+  redcap_projects = redcap_projects,
+  redcap_user_rights = redcap_user_rights,
+  redcap_user_roles = redcap_user_roles,
+  redcap_entity_project_ownership = redcap_entity_project_ownership,
+  redcap_project_last_users = redcap_project_last_users
+)
+
+usethis::use_data(cleanup_project_ownership_test_data, overwrite = T)
