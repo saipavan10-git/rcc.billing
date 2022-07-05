279f01c5a89ee1d69e82ee28b138b9d9850d5fe4
Philip Chase <pbc@ufl.edu>
2022-07-05 13:58:12 -0400

    Add ctsit_staff_employment_periods and redesign ctsit_staff

--- data-raw/ctsit_staff.R
+++ data-raw/ctsit_staff.R
@@ -1,17 +1,33 @@
-# Recommended approach from https://www.rdocumentation.org/packages/usethis/versions/2.1.5/topics/use_data
 library(tibble)
 library(usethis)
+library(lubridate)
 
 ctsit_staff <- tribble(
   ~redcap_username,
-  ~employment_intervals,
-  "pbc", '[ {start: "2011-05-01", end : ""} ]',
-  "tls", '[ {start: "2014-01-01", end : ""} ]',
-  "cpb", '[ {start: "2011-05-01", end : ""} ]',
-  "mbentz", '[ {start: "2020-12-01", end : ""} ]',
-  "kyle.chesney", '[ {start: "2019-04-01", end : ""} ]',
+  "pbc",
+  "tls",
+  "cpb",
+  "mbentz",
+  "kyle.chesney",
+  "taeber"
 )
 
 usethis::use_data(ctsit_staff, overwrite = TRUE)
 
 # sinew::makeOxygen(ctsit_staff)
+
+ctsit_staff_employment_periods <- tribble(
+  ~redcap_username,
+  ~employment_interval,
+  "pbc", lubridate::interval(ymd("2011-05-01"), ymd("2100-01-01")),
+  "tls", lubridate::interval(ymd("2014-01-01"), ymd("2100-01-01")),
+  "cpb", lubridate::interval(ymd("2011-05-11"), ymd("2100-01-01")),
+  "mbentz", lubridate::interval(ymd("2020-12-01"), ymd("2100-01-01")),
+  "kyle.chesney", lubridate::interval(ymd("2019-04-01"), ymd("2100-01-01")),
+  "taeber", lubridate::interval(ymd("2000-01-01"), ymd("2000-01-02")),
+  "taeber", lubridate::interval(ymd("2001-01-01"), ymd("2001-01-02"))
+)
+
+usethis::use_data(ctsit_staff_employment_periods, overwrite = TRUE)
+
+# sinew::makeOxygen(ctsit_staff_employment_periods)
