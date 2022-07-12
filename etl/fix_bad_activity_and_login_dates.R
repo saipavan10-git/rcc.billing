42a01a01fd093e855f509074394063e1c7bc8114
Philip Chase <pbc@ufl.edu>
2022-07-12 13:55:39 -0400

    Improve sharability of fix_bad_activity_and_login_dates.R

--- etl/fix_bad_activity_and_login_dates.R
+++ etl/fix_bad_activity_and_login_dates.R
@@ -6,42 +6,51 @@ library(lubridate)
 library(tidyverse)
 
 load_dot_env("prod.env")
-init_etl("fix_bad_activity_and_login_dates")
+redcapcustodian::init_etl("fix_bad_activity_and_login_dates")
+# This script fixes the data bug induced by the WUPS code bug described in issue #23,
+# https://github.com/ctsit/warn_users_of_pending_suspension/issues/23
 
 rc_conn <- connect_to_redcap_db()
+
+# get fact we need from REDCap's config
+suspend_users_inactive_days <- tbl(rc_conn, "redcap_config") %>%
+  filter(field_name == "suspend_users_inactive_days") %>%
+  collect() %>%
+  pull(value)
 
 # get the data
 redcap_user_information <- tbl(rc_conn, "redcap_user_information") %>%
   collect() %>%
-  filter(!ui_id %in% c(1,2))
+  filter(!ui_id %in% c(1, 2))
 
 users_who_should_have_been_suspended <- redcap_user_information %>%
   filter(is.na(user_suspended_time)) %>%
   filter(user_lastlogin == user_lastactivity) %>%
-  filter(user_lastactivity <= now() - ddays(180))
+  filter(user_lastactivity <= now() - ddays(suspend_users_inactive_days))
 
 distribution_of_age_in_months <- users_who_should_have_been_suspended %>%
-  mutate(months_old = round((now() - user_lastactivity)/ddays(30))) %>%
+  mutate(months_old = round((now() - user_lastactivity) / ddays(30))) %>%
   count(months_old)
 
-users_to_suspend_without_a_second_thought <-
-  users_who_should_have_been_suspended %>%
-  filter(user_lastactivity <= now() - ddays(270)) %>%
+users_to_suspend_without_a_second_thought <- users_who_should_have_been_suspended %>%
+  filter(user_lastactivity <= now() - ddays(suspend_users_inactive_days + 90)) %>%
   mutate(user_lastactivity = user_lastlogin - ddays(1)) %>%
   mutate(reason = "users_to_suspend_without_a_second_thought") %>%
   select(reason, ui_id, user_lastactivity, user_lastlogin)
 
+# Note: the fuzz I add to user_lastlogin in this dataframe was chosen to suit
+# my needs at UF. You might want to tailor this according to your needs.
+#  -- Philip
 users_who_deserve_some_grace <- users_who_should_have_been_suspended %>%
   anti_join(users_to_suspend_without_a_second_thought, by = "ui_id") %>%
   rowwise() %>%
-  mutate(user_lastlogin = now()  - ddays(180)
-         + ddays(sample(seq(4, 17, by = 1), size = 1, replace = TRUE))) %>%
+  mutate(user_lastlogin = now() - ddays(suspend_users_inactive_days)
+    + ddays(sample(seq(4, 17, by = 1), size = 1, replace = TRUE))) %>%
   ungroup() %>%
   mutate(reason = "users_who_deserve_some_grace") %>%
   select(reason, ui_id, user_lastactivity, user_lastlogin)
 
-active_users_who_need_an_activity_tweak <-
-  redcap_user_information %>%
+active_users_who_need_an_activity_tweak <- redcap_user_information %>%
   filter(is.na(user_suspended_time)) %>%
   anti_join(users_to_suspend_without_a_second_thought, by = "ui_id") %>%
   anti_join(users_who_deserve_some_grace, by = "ui_id") %>%
@@ -73,7 +82,7 @@ update_n <- sync_table(
 )
 
 if (nrow(all_dataset_diff$update_records) == update_n) {
-  log_job_success(jsonlite::toJSON(all_dataset_diff))
+  redcapcustodian::log_job_success(jsonlite::toJSON(all_dataset_diff))
 } else {
-  log_job_failure(jsonlite::toJSON(all_dataset_diff))
+  redcapcustodian::log_job_failure(jsonlite::toJSON(all_dataset_diff))
 }
