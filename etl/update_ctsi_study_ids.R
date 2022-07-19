dfde871316e7c093efbe38af00a76980bccd500c
Philip Chase <pbc@ufl.edu>
2022-07-19 15:05:42 -0400

    Update update_ctsi_study_ids.R
    Add testing comments.
    Fix table sync logic.
    Express activity_log as a list of dataframes.

--- etl/update_ctsi_study_ids.R
+++ etl/update_ctsi_study_ids.R
@@ -10,12 +10,39 @@ init_etl("update_ctsi_study_ids")
 
 rcc_billing_conn <- connect_to_rcc_billing_db()
 
+# # uncomment this code to load test data into an in-memory DB
+# conn <- DBI::dbConnect(RSQLite::SQLite(), dbname = ":memory:")
+#
+# rcc_billing_conn <- conn
+#
+# table_names <- c(
+#   "redcap_entity_project_ownership",
+#   "redcap_projects",
+#   "service_instance",
+#   "invoice_line_item",
+#   "service_type",
+#   "invoice_line_item_communications"
+# )
+#
+# for (table_name in table_names) {
+#   create_and_load_test_table(
+#     table_name = table_name,
+#     conn = conn,
+#     load_test_data = T,
+#     is_sqllite = T
+#   )
+# }
+
+
 initial_service_instance <- tbl(rcc_billing_conn, "service_instance") %>%
-  ## head(3) %>%
+  # # uncomment this code to simulate missing data
+  # head(3) %>%
   collect()
 
-## ctsi_study_ids <- initial_service_instance %>%
-##   tail(3)
+# # run this code in lieu of the next block to simulate missing data
+# ctsi_study_ids <- tbl(rcc_billing_conn, "service_instance") %>%
+#   collect() %>%
+#   tail(3)
 
 ctsi_study_ids <- read_csv("./output/ctsi_study_ids.csv") %>%
   janitor::clean_names() %>%
@@ -42,27 +69,29 @@ service_instance_sync_activity <- redcapcustodian::sync_table(
 )
 
 updated_service_instance <- tbl(rcc_billing_conn, "service_instance") %>%
-  ## head(6) %>%
+  # # uncomment this code to simulate missing data
+  # head(6) %>%
   collect()
 
 # Update invoice_line_items ###################################################
 
 initial_invoice_line_items <- tbl(rcc_billing_conn, "invoice_line_item") %>%
-  # randomly assign NA for testing
-  ## collect() %>%
-  ## mutate(ctsi_study_id = if_else(
-  ##   sample(c(T, F), n(), replace = TRUE),
-  ##   NA_real_,
-  ##   ctsi_study_id
-  ##   )
-  ## ) %>%
+  # # uncomment this code to simulate missing data
+  # # randomly assign NA for testing
+  # collect() %>%
+  # mutate(ctsi_study_id = if_else(
+  #   sample(c(T, F), n(), replace = TRUE),
+  #   NA_real_,
+  #   ctsi_study_id
+  #   )
+  # ) %>%
   filter(is.na(ctsi_study_id)) %>%
   collect()
 
 invoice_line_item_updates <- initial_invoice_line_items %>%
   inner_join(updated_service_instance, by = c("service_identifier" = "service_instance_id")) %>%
   mutate(ctsi_study_id = ctsi_study_id.y) %>%
-  select(-ends_with(c(".x", ".y")))
+  select(-ends_with(c(".x", ".y")), -active)
 
 invoice_line_item_diff <-
   redcapcustodian::dataset_diff(
@@ -79,14 +108,18 @@ invoice_line_item_sync_activity <- redcapcustodian::sync_table(
   table_name = "invoice_line_item",
   primary_key = "service_identifier",
   data_diff_output = invoice_line_item_diff,
-  insert = T,
-  update = F,
+  insert = F,
+  update = T,
   delete = F
 )
 
-activity_log <- bind_rows(
-  service_instances_diff$insert_records,
-  invoice_line_item_sync_activity$update_records
+activity_log <- list(
+  service_instance = service_instances_diff$insert_records %>%
+    mutate(diff_type = "insert") %>%
+    select(diff_type, everything()),
+  invoice_line_item = invoice_line_item_diff$update_records %>%
+    mutate(diff_type = "update") %>%
+    select(diff_type, everything())
 )
 
 log_job_success(jsonlite::toJSON(activity_log))
