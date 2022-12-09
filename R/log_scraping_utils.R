e7e799d6fedb8fdbb3c2b4f91a682d8d4117cec2
Philip Chase <pbc@ufl.edu>
2022-12-09 12:13:04 -0500

    Update get_bad_emails_from_log
    Add a log_date filter.
    Specified job logs to read.
    Fixed bug excluding sequester_orphans jobs.
    Uncommented final error filter.

--- R/log_scraping_utils.R
+++ R/log_scraping_utils.R
@@ -12,18 +12,35 @@ get_bad_emails_from_log <- function() {
 
   job_log <- tbl(log_conn, "rcc_job_log") %>%
     dplyr::filter(str_detect(.data$job_summary_data, "Recipient address rejected: User unknown in virtual alias table")) %>%
+    dplyr::filter(script_name %in% c("warn_owners_of_impending_bill", "sequester_orphans")) %>%
+    filter(log_date > local(get_script_run_time() - ddays(31))) %>%
     collect()
 
-  result <- job_log %>%
-    dplyr::arrange(dplyr::desc(.data$id)) %>%
-    dplyr::pull(.data$job_summary_data) %>%
-    utils::head(1) %>%
-    jsonlite::fromJSON()
+  if (nrow(job_log) > 0) {
+    job_summaries <- job_log %>%
+      dplyr::arrange(dplyr::desc(.data$id)) %>%
+      dplyr::pull(.data$job_summary_data)
 
-  bad_recipients <- result$billing_alert_log %>%
-    ## filter(str_detect(error_message, "Recipient address rejected: User unknown in virtual alias table")) %>%
-    select(email = recipient) %>%
-    distinct()
+    billing_alert_logs <- tribble(
+      ~recipient, ~projects, ~error_message,
+      "asdf", "asdf", "asdf"
+    ) %>% filter(F)
+
+    for (job in seq(1,length(job_summaries))) {
+      job_summary <- jsonlite::fromJSON(job_summaries[job])$billing_alert_log
+      message("converting job ", job)
+      billing_alert_logs <- bind_rows(df, job_summary)
+    }
+
+    bad_recipients <- billing_alert_logs %>%
+      filter(str_detect(error_message, "Recipient address rejected: User unknown in virtual alias table")) %>%
+      select(email = recipient) %>%
+      distinct()
+  } else {
+    bad_recipients <- tribble(
+      ~email, "asdf"
+    ) %>% filter(F)
+  }
 
   return(bad_recipients)
 }
