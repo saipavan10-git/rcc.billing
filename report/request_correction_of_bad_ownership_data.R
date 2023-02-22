3ba4932619d64b5b38e2dc5e00abca4394735f28
Philip Chase <pbc@ufl.edu>
2023-02-22 15:36:22 -0500

    Update request_correction_of_bad_ownership_data.R
    Add test data, a script to make it and a script to load it.
    Replace testing comments in request_correction_of_bad_ownership_data.R to accommodate test data.
    Set email text and subject lines.
    Add support for new replacement strings in the email templates.
    Fix bugs in the date comparisons.

--- report/request_correction_of_bad_ownership_data.R
+++ report/request_correction_of_bad_ownership_data.R
@@ -14,19 +14,24 @@ init_etl("request_correction_of_bad_ownership_data")
 
 rc_conn <- connect_to_redcap_db()
 
+# For testing, run the script tests/testhat/request_correction_of_bad_ownership/load_test_data_for_request_correction_of_bad_ownership.R
+#   That script loads data generated and saved by tests/testhat/request_correction_of_bad_ownership/make_test_data_for_request_correction_of_bad_ownership.R
+# Then run this line:
+# rc_conn <- conn
+# ...and the rcui line below
+
 rcp <- tbl(rc_conn, "redcap_projects")
 rcpo <- tbl(rc_conn, "redcap_entity_project_ownership")
 rcur <- tbl(rc_conn, "redcap_user_rights")
 rcui <- tbl(rc_conn, "redcap_user_information")
+# Run the line below when testing to make email go to you instead of bouncing
+# rcui <- tbl(rc_conn, "redcap_user_information") %>%
+#    mutate(across(starts_with("user_email"), ~ if_else(is.na(.), ., paste0(Sys.getenv("USER"), "@ufl.edu"))))
 
 redcap_version <- tbl(rc_conn, "redcap_config") %>%
   filter(field_name == "redcap_version") %>%
   collect(value) %>%
   pull()
-# Local testing overrides
-## redcap_version <- "11.3.4"
-## your_email1 <- "foo@bar.edu"
-## your_email2 <- "foo@bar.com"
 
 redcap_project_uri_base <- str_remove(Sys.getenv("URI"), "/api") %>%
   paste0("redcap_v", redcap_version, "/ProjectSetup/index.php?pid=")
@@ -44,23 +49,10 @@ projects_with_unresolvable_ownership_issues <- rcp %>%
   filter(is.na(email) || email == "") %>%
   filter(is.na(firstname) || firstname == "") %>%
   filter(is.na(lastname) || lastname == "") %>%
-  # NOTE: as of this PR this filter results in an empty set
-  ## filter(is.na(username) || username == "") %>%
+  filter(is.na(username) || username == "") %>%
   collect() %>%
   mutate_columns_to_posixct("updated") %>%
-  filter(get_script_run_time() - ddays(120) > updated)
-
-## HACK: inject a "sequestered today" for local testing
-## hack1 <- projects_with_unresolvable_ownership_issues %>%
-##   tail(1) %>%
-##   mutate(
-##     project_name = "hack1",
-##     sequestered = 1,
-##     updated = get_script_run_time()
-##   )
-## projects_with_unresolvable_ownership_issues <- projects_with_unresolvable_ownership_issues %>%
-##   head(5) %>%
-##   rbind(hack1)
+  filter(get_script_run_time() - ddays(120) < updated)
 
 # ["foo", "", NA, "bar", NA] -> "foo,bar"
 # https://stackoverflow.com/a/49201394/7418735
@@ -70,7 +62,7 @@ project_contact_information <- rcur %>%
   filter(project_id %in% local(projects_with_unresolvable_ownership_issues$project_id)) %>%
   inner_join(rcp, by = "project_id") %>%
   inner_join(rcui, by = "username") %>%
-  select(project_id, username, starts_with("user_email"), app_title, design) %>%
+  select(project_id, username, user_firstname, user_lastname, starts_with("user_email"), app_title, design) %>%
   collect() %>%
   mutate(link_to_project = paste0(redcap_project_uri_base, project_id)) %>%
   mutate(app_title = str_replace_all(app_title, '"', "")) %>%
@@ -136,16 +128,23 @@ send_alert_email <- function(row, email_subject = "") {
 
 # these are projects that had their ownership data erased in the past 3 months
 please_fix <- projects_with_unresolvable_ownership_issues %>%
-  filter(is.na(sequestered) || sequestered == 0) %>%
-  filter(get_script_run_time() - dmonths(3) > updated)
+  filter(is.na(sequestered) | sequestered == 0) %>%
+  filter(get_script_run_time() - dmonths(3) < updated)
 
-please_fix_email_template_text <- str_replace( "<p>Hello,<p>
-You are being contacted about <app_title>,
-<p><a href=\"<redcap_project_ownership_page>\">REDCap Project Ownership</a>.</p>
+please_fix_email_template_text <- "<p>Hello,</p>
+<p>The REDCap project listed below has been identified as having out-of-date PI or owner information. Emails to PI/Owner listed on this project have been bouncing. We have erased those bad emails addresses, but a correct email address is needed to address billing issues. Please go into the Main Project Settings and update the PI/ownership information at <project_hyperlink>. </p>
+
+<p>If this is not corrected within the next 30 days, the project will be sequestered and you will no longer have access to it. If the project is still sequestered one year from now, the project will be deleted at that time.</p>
+
+<p>We are sending this email to everyone who has design rights on this REDCap project. Please confer with them if you are unsure who the Owner/PI is. Those people are:</p>
+
 <users_with_design_rights>
-<link_to_project>,",
-"<redcap_project_ownership_page>",
-redcap_project_ownership_page)
+
+<p>If you no longer need or use this REDCap project, we encourage you to export your project design and your project data, and delete the project. To delete a project, access its project link above then follow the instructions in <a href=\"https://www.ctsi.ufl.edu/files/2018/04/How-to-Delete-a-Project-in-REDCap.pdf\">Deleting a Project in REDCap</a>.</p>
+<p>Instructions on how to update the PI/Owner can be found at <a href=\"https://www.ctsi.ufl.edu/files/2018/04/How-to-Update-Project-Ownership-Info-PI-Information-and-IRB-Number.pdf\">Update Project Ownership, PI Name & Email and IRB Number in REDCap</a>.</p>
+
+<p>Regards,<br>REDCap Support</p>
+<p>This message was sent from an unmonitored mailbox. If you have questions, please open a <a href=\"https://redcap.ctsi.ufl.edu/redcap/surveys/?s=DUPrXGmx3L\">REDCap Service/Consultation Request</a>.</p>"
 
 please_fix_contacts <- project_contact_information %>%
   filter(project_id %in% please_fix$project_id) %>%
@@ -159,13 +158,13 @@ please_fix_email_df <- please_fix_contacts %>%
   mutate(email_text =
            str_replace(please_fix_email_template_text, "<app_title>", app_title) %>%
            str_replace("<link_to_project>", link_to_project) %>%
+           str_replace("<project_hyperlink>", project_hyperlink) %>%
+           str_replace("<project_id>", as.character(project_id)) %>%
            str_replace("<users_with_design_rights>", users_with_design_rights)
          )
-  # uncomment for local testing
-  ## mutate(emails = paste(your_email1, your_email2, sep = " "))
 
 send_please_fix_alert_email <- function(row) {
-  return(send_alert_email(row, email_subject = "Please Fix"))
+  return(send_alert_email(row, email_subject = "Please fix the PI or Ownership of this REDCap Project"))
 }
 
 please_fix_log <- apply(
@@ -187,12 +186,13 @@ sequestered_today <- projects_with_unresolvable_ownership_issues %>%
   # updated this morning (i.e. today)
   filter(date(get_script_run_time()) == date(updated))
 
-sequestered_today_email_template_text <- str_replace( "<p>Hello,<p>
-You are being contacted about <app_title>,
-<p><a href=\"<redcap_project_ownership_page>\">REDCap Project Ownership</a>.</p>
-<link_to_project>,",
-"<redcap_project_ownership_page>",
-redcap_project_ownership_page)
+sequestered_today_email_template_text <- "<p>Hello,<p>
+<p>The REDCap project, <i><app_title></i> with project ID <i><project_id></i>, has been sequestered because it appears to have been abandoned. We are happy to unsequester the project if that assessment is incorrect. If you still need access to it, please open a <a href=\"https://redcap.ctsi.ufl.edu/redcap/surveys/?s=DUPrXGmx3L\">REDCap Service/Consultation Request</a> telling us which project(s) need to be unsequestered. Please also provide updated PI/ownership information for the project. The PI/Owner is the person who can approve payment for fees related to this project.
+
+<p>If you take no action, this project will remain inaccessible. If it is still sequestered one year from now, it will be deleted at that time.</p>
+
+<p>Regards,<br>REDCap Support</p>
+<p>This message was sent from an unmonitored mailbox. If you have questions, please open a <a href=\"https://redcap.ctsi.ufl.edu/redcap/surveys/?s=DUPrXGmx3L\">REDCap Service/Consultation Request</a>.</p>"
 
 sequestered_today_contacts <- project_contact_information %>%
   filter(project_id %in% sequestered_today$project_id) %>%
@@ -202,14 +202,14 @@ sequestered_today_email_df <- sequestered_today_contacts %>%
   rowwise() %>%
   mutate(email_text =
            str_replace(sequestered_today_email_template_text, "<app_title>", app_title) %>%
+           str_replace("<project_hyperlink>", project_hyperlink) %>%
+           str_replace("<project_id>", as.character(project_id)) %>%
            str_replace("<link_to_project>", link_to_project)
          ) %>%
   ungroup()
-  # uncomment for local testing
-  ## mutate(emails = paste(your_email1, your_email2, sep = " "))
 
 send_sequestered_today_alert_email <- function(row) {
-  return(send_alert_email(row, email_subject = "Sequestered Today"))
+  return(send_alert_email(row, email_subject = "REDCap project sequestered"))
 }
 
 sequestered_today_log <- apply(
@@ -234,5 +234,3 @@ activity_log <- bind_rows(
 log_job_success(jsonlite::toJSON(activity_log))
 
 dbDisconnect(rc_conn)
-
-Create initial version of request_correction_of_bad_ownership_data report
