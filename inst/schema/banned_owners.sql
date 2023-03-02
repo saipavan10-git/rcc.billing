4948e377a568014b912d8dd9dfee1756cc448631
Kyle Chesney <kyle.2493@gmail.com>
2023-03-02 14:37:52 -0500

    Remove deprecated integer size limit in banned_owners id
    Co-authored-by: Philip Chase <pbc@ufl.edu>

--- inst/schema/banned_owners.sql
+++ inst/schema/banned_owners.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `banned_owners` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
   `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `date_added` datetime NOT NULL,
