c5973d5e9c25699700c2a40d392ce5dee1515977
Philip Chase <pbc@ufl.edu>
2024-05-22 17:20:20 -0400

    Fix get_probono_service_request_records()
    Use multiple small test of increasing complexity.
    Revise get_probono_service_request_records() until is passes all of the new tests.

--- R/get_probono_service_request_records.R
+++ R/get_probono_service_request_records.R
@@ -2,7 +2,7 @@
 #'
 #' This function processes a dataframe of service requests to update the billable_rate field
 #'
-#' @param service_requests A dataframe containing service request data form REDCap PID 1414
+#' @param service_requests A dataframe containing service request data from REDCap PID 1414
 #'
 #' @return Returns a dataframe with updated billable rate
 #'
@@ -12,16 +12,19 @@
 #' }
 #' @export
 get_probono_service_request_updates <- function(service_requests) {
-  probono_updates <- service_requests |>
+  non_probono_records_that_should_be_probono <-
+    service_requests |>
     dplyr::arrange(
       .data$record_id,
       .data$redcap_repeat_instrument,
-      .data$redcap_repeat_instance) |>
+      .data$redcap_repeat_instance
+    ) |>
     # fill project ids on each record group
     dplyr::group_by(.data$record_id) |>
     tidyr::fill("project_id", .direction = "updown") |>
     dplyr::ungroup() |>
     dplyr::filter(!is.na(.data$project_id)) |>
+    dplyr::filter(!is.na(.data$redcap_repeat_instrument)) |>
     # Compute time, and time total for each project_id-probono group
     dplyr::mutate(time = service_request_time(.data$time2, .data$time_more)) |>
     dplyr::mutate(probono = (.data$billable_rate == 0)) |>
@@ -32,32 +35,75 @@ get_probono_service_request_updates <- function(service_requests) {
       "project_id",
       "probono",
       "time",
-      "billable_rate"
+      "billable_rate",
+      dplyr::everything()
     ) |>
     dplyr::group_by(.data$project_id, .data$probono) |>
-    dplyr::mutate(total_time = sum(.data$time)) |>
+    dplyr::mutate(total_time_by_project = sum(.data$time)) |>
     dplyr::ungroup() |>
     # Copy probono time to each record across a project_id group
     dplyr::group_by(.data$project_id) |>
     dplyr::mutate(probono_time = dplyr::case_when(
-      .data$probono ~ .data$total_time,
-      TRUE ~ 0)) |>
+      .data$probono ~ .data$total_time_by_project,
+      TRUE ~ 0
+    )) |>
     dplyr::mutate(probono_time = max(.data$probono_time)) |>
+    # eliminate records that have already been marked as probono
+    dplyr::filter(.data$billable_rate != 0) |>
     # Mark additional records as probono as needed
     dplyr::filter(.data$probono_time < 1) |>
     dplyr::arrange(.data$time) |>
-    dplyr::mutate(additional_time = cumsum(.data$time)) |>
-    dplyr::filter(.data$additional_time + .data$probono_time >= 1) |>
-    dplyr::filter(.data$additional_time == min(.data$additional_time)) |>
-    dplyr::mutate(billable_rate = 0) |>
-    dplyr::ungroup() |>
-    # keep only enough columns to update the service request DB
-    dplyr::select(
-      "record_id",
-      "redcap_repeat_instrument",
-      "redcap_repeat_instance",
-      "billable_rate")
+    dplyr::mutate(draft_probono_time = cumsum(.data$time) + .data$probono_time) |>
+    dplyr::mutate(sufficient_draft_probono_time = (.data$draft_probono_time >= 1)) |>
+    dplyr::ungroup()
+
+  if (nrow(non_probono_records_that_should_be_probono) == 0) {
+    # Prevent -Inf warnings form min when non_probono_records_that_should_be_probono is empty
+    probono_updates <- non_probono_records_that_should_be_probono |>
+      dplyr::ungroup() |>
+      # keep only enough columns to update the service request DB
+      dplyr::select(
+        "record_id",
+        "redcap_repeat_instrument",
+        "redcap_repeat_instance",
+        "billable_rate"
+      )
+  } else {
+    max_row_num_by_project <- dplyr::bind_rows(
+      # projects where there is sufficient_draft_probono_time
+      non_probono_records_that_should_be_probono |>
+        dplyr::group_by(.data$project_id) |>
+        dplyr::mutate(row_num = dplyr::row_number()) |>
+        dplyr::filter(.data$sufficient_draft_probono_time) |>
+        dplyr::slice_head(n = 1) |>
+        dplyr::ungroup() |>
+        dplyr::select("project_id", max_row_num_by_project = "row_num"),
+      # All projects
+      non_probono_records_that_should_be_probono |>
+        dplyr::group_by(.data$project_id) |>
+        dplyr::mutate(row_num = dplyr::row_number()) |>
+        dplyr::filter(!.data$sufficient_draft_probono_time) |>
+        dplyr::slice_tail(n = 1) |>
+        dplyr::ungroup() |>
+        dplyr::select("project_id", max_row_num_by_project = "row_num")
+    ) |>
+      dplyr::arrange(dplyr::desc(.data$max_row_num_by_project)) |>
+      dplyr::distinct(.data$project_id, .keep_all = T)
+
+    probono_updates <- non_probono_records_that_should_be_probono |>
+      dplyr::group_by(.data$project_id) |>
+      dplyr::left_join(max_row_num_by_project, by = "project_id") |>
+      dplyr::filter(dplyr::row_number() <= max_row_num_by_project) |>
+      dplyr::mutate(billable_rate = 0) |>
+      dplyr::ungroup() |>
+      # keep only enough columns to update the service request DB
+      dplyr::select(
+        "record_id",
+        "redcap_repeat_instrument",
+        "redcap_repeat_instance",
+        "billable_rate"
+      )
+  }
 
   return(probono_updates)
 }
-
--- tests/testthat/test-get_probono_service_request_updates.R
+++ tests/testthat/test-get_probono_service_request_updates.R
@@ -1,24 +1,30 @@
-# records 1128 and 4907 contain duplicated project id.The record with the least time
-# will be updated by get_probono_service_request_updates in addition to record_id 3
-# which contians a unique project_id.
-testthat::test_that("get_probono_service_request_updates returns the correct df", {
+testthat::test_that("get_probono_service_request_updates handles single records with one repeat, no existing probono and time <= 1 hour", {
   service_requests <- data.frame(
-    record_id = rep(c(3, 66, 1128, 4907, 6458, 6473, 5883), each = 2),
-    redcap_repeat_instrument = rep(c(NA, "help_desk_response"), 7),
-    redcap_repeat_instance = rep(c(NA, 1), 7),
-    project_id = c(851, NA, NA, NA, 6058, NA, 6058, NA, 12034, NA, 13433, NA, 10172, NA),
-    time2 = c(NA, 45, NA, 15, NA, 30, NA, 15, NA, 30, NA, 15, NA, 75),
-    time_more = c(rep(NA, 13), 3),
-    billable_rate = c(NA, 0, rep(NA, 3), 0, NA, 0, NA, 130, NA, 0, NA, 0)
+    record_id = rep(seq(1,4), each = 2),
+    redcap_repeat_instrument = rep(c(NA, "help_desk_response"), 4),
+    redcap_repeat_instance = rep(c(NA, 1), 4),
+    project_id = c(
+      10, NA,
+      20, NA,
+      30, NA,
+      40, NA
+    ),
+    time2 = c(
+      NA, 15,
+      NA, 30,
+      NA, 45,
+      NA, 60
+    ),
+    time_more = c(rep(NA, 8)),
+    billable_rate = rep(c(NA, 130), 4)
   )
 
-  expected_output <- data.frame(
-    record_id = c(4907, 3),
-    redcap_repeat_instrument = c("help_desk_response", "help_desk_response"),
-    redcap_repeat_instance = c(1, 1),
-    billable_rate = c(0, 0)
-  ) |>
-    as_tibble()
+  expected_output <- dplyr::tibble(
+    record_id = seq(1,4),
+    redcap_repeat_instrument = rep("help_desk_response", 4),
+    redcap_repeat_instance = rep(1, 4),
+    billable_rate = rep(0, 4)
+  )
 
   testthat::expect_equal(
     get_probono_service_request_updates(service_requests),
@@ -26,3 +32,231 @@ testthat::test_that("get_probono_service_request_updates returns the correct df"
   )
 
 })
+
+testthat::test_that("get_probono_service_request_updates handles single records with one repeat, all existing time being probono, and time <= 1 hour", {
+  service_requests <- data.frame(
+    record_id = rep(seq(1,4), each = 2),
+    redcap_repeat_instrument = rep(c(NA, "help_desk_response"), 4),
+    redcap_repeat_instance = rep(c(NA, 1), 4),
+    project_id = c(
+      10, NA,
+      20, NA,
+      30, NA,
+      40, NA
+    ),
+    time2 = c(
+      NA, 15,
+      NA, 30,
+      NA, 45,
+      NA, 60
+    ),
+    time_more = c(rep(NA, 8)),
+    billable_rate = rep(c(NA, 0), 4)
+  )
+
+  # expected_output is an empty data frame
+  testthat::expect_equal(
+    nrow(get_probono_service_request_updates(service_requests)),
+    0
+  )
+
+})
+
+testthat::test_that("get_probono_service_request_updates handles single records with one repeat, 50% probono and time <= 1 hour", {
+  service_requests <- data.frame(
+    record_id = rep(seq(1,4), each = 2),
+    redcap_repeat_instrument = rep(c(NA, "help_desk_response"), 4),
+    redcap_repeat_instance = rep(c(NA, 1), 4),
+    project_id = c(
+      10, NA,
+      20, NA,
+      30, NA,
+      40, NA
+    ),
+    time2 = c(
+      NA, 15,
+      NA, 30,
+      NA, 45,
+      NA, 60
+    ),
+    time_more = c(rep(NA, 8)),
+    billable_rate = rep(c(NA, 130, NA, 0), 2)
+  )
+
+  expected_output <- dplyr::tibble(
+    record_id = c(1,3),
+    redcap_repeat_instrument = rep("help_desk_response", 2),
+    redcap_repeat_instance = rep(1, 2),
+    billable_rate = rep(0, 2)
+  )
+
+  testthat::expect_equal(
+    get_probono_service_request_updates(service_requests),
+    expected_output
+  )
+
+})
+
+testthat::test_that("get_probono_service_request_updates handles multiple helpdesk requests for a project, where nothing is yet probono", {
+  service_requests <- data.frame(
+    record_id = rep(seq(1,4), each = 2),
+    redcap_repeat_instrument = rep(c(NA, "help_desk_response"), 4),
+    redcap_repeat_instance = rep(c(NA, 1), 4),
+    project_id = c(
+      10, NA,
+      10, NA,
+      10, NA,
+      10, NA
+    ),
+    time2 = c(
+      NA, 15,
+      NA, 30,
+      NA, 45,
+      NA, 60
+    ),
+    time_more = c(rep(NA, 8)),
+    billable_rate = rep(c(NA, 130, NA, 130), 2)
+  )
+
+  expected_output <- dplyr::tibble(
+    record_id = c(1,2,3),
+    redcap_repeat_instrument = rep("help_desk_response", 3),
+    redcap_repeat_instance = rep(1, 3),
+    billable_rate = rep(0, 3)
+  )
+
+  testthat::expect_equal(
+    get_probono_service_request_updates(service_requests),
+    expected_output
+  )
+
+})
+
+testthat::test_that("get_probono_service_request_updates handles multiple time entries on one helpdesk request for a project, where some entries are already probono", {
+  service_requests <- data.frame(
+    record_id = rep(1, 5),
+    redcap_repeat_instrument = c(NA, rep("help_desk_response", 4)),
+    redcap_repeat_instance = c(NA, seq(1,4)),
+    project_id = c(10, rep(NA, 4)),
+    time2 = c(NA, 15, 15, 45, 75),
+    time_more = c(rep(NA, 4), 1.5),
+    billable_rate = c(NA, 0, 130, 130, 130)
+  )
+
+  expected_output <- dplyr::tibble(
+    record_id = c(1,1),
+    redcap_repeat_instrument = rep("help_desk_response", 2),
+    redcap_repeat_instance = c(2, 3),
+    billable_rate = c(0, 0)
+  )
+
+  testthat::expect_equal(
+    get_probono_service_request_updates(service_requests),
+    expected_output
+  )
+
+})
+
+testthat::test_that("get_probono_service_request_updates handles multiple requests with multiple time entries for 3 projects, where some entries are already probono", {
+  service_requests <- data.frame(
+    record_id = c(rep(1, 5), rep(2, 5), rep(3, 5)),
+    redcap_repeat_instrument = rep(c(NA, rep("help_desk_response", 4)), 3),
+    redcap_repeat_instance = rep(c(NA, seq(1, 4)), 3),
+    project_id = c(
+      10, rep(NA, 4),
+      20, rep(NA, 4),
+      10, rep(NA, 4)
+    ),
+    time2 = c(
+      NA, 15, 15, 15, 15,
+      NA, 15, 15, 15, 15,
+      NA, 15, 15, 45, 75
+    ),
+    time_more = c(
+      NA, NA, NA, NA, NA,
+      NA, NA, NA, NA, NA,
+      NA, NA, NA, NA, 3.5
+    ),
+    billable_rate = c(
+      NA, 0, 130, 130, 130,
+      NA, 0, 0, 130, 130,
+      NA, 130, 130, 130, 130
+    )
+  )
+
+  expected_output <- dplyr::tibble(
+    record_id = c(1, 1, 1, 2, 2),
+    redcap_repeat_instrument = rep("help_desk_response", 5),
+    redcap_repeat_instance = c(2, 3, 4, 3, 4),
+    billable_rate = rep(0, 5)
+  )
+
+  testthat::expect_equal(
+    get_probono_service_request_updates(service_requests),
+    expected_output
+  )
+
+})
+
+testthat::test_that("get_probono_service_request_updates handles multiple requests with multiple time entries for 3 projects, where some entries are already probono. Also add non-project entries", {
+  service_requests <-
+    bind_rows(
+      data.frame(
+        record_id = c(rep(1, 5), rep(2, 5), rep(3, 5)),
+        redcap_repeat_instrument = rep(c(NA, rep("help_desk_response", 4)), 3),
+        redcap_repeat_instance = rep(c(NA, seq(1, 4)), 3),
+        project_id = c(
+          10, rep(NA, 4),
+          20, rep(NA, 4),
+          10, rep(NA, 4)
+        ),
+        time2 = c(
+          NA, 15, 15, 15, 15,
+          NA, 15, 15, 15, 15,
+          NA, 15, 15, 45, 75
+        ),
+        time_more = c(
+          NA, NA, NA, NA, NA,
+          NA, NA, NA, NA, NA,
+          NA, NA, NA, NA, 3.5
+        ),
+        billable_rate = c(
+          NA, 0, 130, 130, 130,
+          NA, 0, 0, 130, 130,
+          NA, 130, 130, 130, 130
+        )
+      ),
+      # Add non-project requests
+      data.frame(
+        record_id = rep(c(4, 5, 6), each = 2),
+        redcap_repeat_instrument = rep(c(NA, "help_desk_response"), 3),
+        redcap_repeat_instance = rep(c(NA, 1), 3),
+        project_id = rep(NA, 6),
+        time2 = c(
+          NA, 15,
+          NA, 75,
+          NA, 45
+        ),
+        time_more = c(
+          NA, NA, NA, 2.5, NA, NA
+        ),
+        billable_rate = c(
+          NA, 0, NA, 130, NA, 130
+        )
+      )
+    )
+
+  expected_output <- dplyr::tibble(
+    record_id = c(1, 1, 1, 2, 2),
+    redcap_repeat_instrument = rep("help_desk_response", 5),
+    redcap_repeat_instance = c(2, 3, 4, 3, 4),
+    billable_rate = rep(0, 5)
+  )
+
+  testthat::expect_equal(
+    get_probono_service_request_updates(service_requests),
+    expected_output
+  )
+
+})
+
