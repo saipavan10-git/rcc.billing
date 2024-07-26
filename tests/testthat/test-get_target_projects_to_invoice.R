121c38fbe8d5a9caf838d0094172409f982a00d9
Philip Chase <pbc@ufl.edu>
2024-07-26 11:06:06 -0400

    Add set_script_run_time to test-get_target_projects_to_invoice.R
    Add package names where they were omitted in related code.

--- tests/testthat/test-get_target_projects_to_invoice.R
+++ tests/testthat/test-get_target_projects_to_invoice.R
@@ -6,10 +6,12 @@ test_that("get_target_projects_to_invoice returns one project to invoice", {
     "redcap_user_information"
   )
 
+  redcapcustodian::set_script_run_time(lubridate::ymd_hms("2023-04-05 12:00:00"))
+
   mem_rc_conn <- DBI::dbConnect(duckdb::duckdb(), dbdir = ":memory:")
   purrr::walk(test_tables, create_a_table_from_rds_test_data, mem_rc_conn, "get_target_projects_to_invoice")
 
   testthat::expect_equal(get_target_projects_to_invoice(mem_rc_conn) |> nrow(), 1)
 
-  dbDisconnect(mem_rc_conn, shutdown=TRUE)
+  DBI::dbDisconnect(mem_rc_conn, shutdown=TRUE)
 })
