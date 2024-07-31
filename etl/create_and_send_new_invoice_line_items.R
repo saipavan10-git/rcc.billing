1141f8472ad9b54feab0d75128cb1679aea266b1
Philip Chase <pbc@ufl.edu>
2024-07-31 16:36:11 -0400

    Add service_request_service_instances in create_and_send_new_invoice_line_items.R
    Resequence code to allow all service_instance updates to happen in one database update.
    Deselect fiscal_contact fields for now.
    Update subject and body of email.

--- etl/create_and_send_new_invoice_line_items.R
+++ etl/create_and_send_new_invoice_line_items.R
@@ -12,22 +12,56 @@ source_credentials <- get_redcap_credentials(Sys.getenv("REDCAP_SERVICE_REQUEST_
 rc_conn <- connect_to_redcap_db()
 rcc_billing_conn <- connect_to_rcc_billing_db()
 
+# get initial state
 initial_invoice_line_item <- tbl(rcc_billing_conn, "invoice_line_item") %>%
   collect()
 
-target_projects <- get_target_projects_to_invoice(rc_conn)
-
-# Make new service_instance rows ##############################################
 initial_service_instance <- tbl(rcc_billing_conn, "service_instance") %>%
   collect()
+
+# Make Service Request line items
+service_requests <- REDCapR::redcap_read(
+  redcap_uri = source_credentials$redcap_uri,
+  token = source_credentials$token,
+  batch_size = 2000
+)$data
+
+service_request_line_items <- get_service_request_line_items(
+  service_requests = service_requests,
+  rc_billing_conn = rcc_billing_conn,
+  rc_conn = rc_conn
+) |>
+# fix incorrect data types
+mutate(across(c("service_type_code", "ctsi_study_id", "price_of_service", "qty_provided", "amount_due"), as.double))
+
+# Derive the service_instances from service_request_line_items
+service_request_service_instances <-
+  service_request_line_items |>
+  mutate(active = 1) |>
+  select(
+    "service_instance_id",
+    "service_type_code",
+    "service_identifier",
+    "ctsi_study_id",
+    "active"
+  )
+
+# Make new service_instance rows ##############################################
+target_projects <- get_target_projects_to_invoice(rc_conn)
 
 new_service_instances <- get_new_project_service_instances(
   projects_to_invoice = target_projects,
   initial_service_instance = initial_service_instance
 )
 
+combined_new_new_service_instances <-
+  bind_rows(
+    new_service_instances,
+    service_request_service_instances
+  )
+
 new_service_instances_diff <- redcapcustodian::dataset_diff(
-  source = new_service_instances,
+  source = combined_new_new_service_instances,
   source_pk = "service_instance_id",
   target = initial_service_instance,
   target_pk = "service_instance_id",
@@ -45,7 +79,7 @@ service_instance_sync_activity <- redcapcustodian::sync_table(
   delete = F
 )
 
-# Create invoice_line_item rows ###############################################
+# Create invoice_line_item rows for projects ###############################################
 new_project_invoice_line_items <- get_new_project_invoice_line_items(
     projects_to_invoice = target_projects,
     initial_invoice_line_item,
@@ -53,24 +87,6 @@ new_project_invoice_line_items <- get_new_project_invoice_line_items(
     rcc_billing_conn,
     api_uri = Sys.getenv("URI")
 )
-
-service_requests <- REDCapR::redcap_read(
-  redcap_uri = source_credentials$redcap_uri,
-  token = source_credentials$token,
-  batch_size = 2000
-)$data
-
-service_request_line_items <- get_service_request_line_items(
-  service_requests = service_requests,
-  rc_billing_conn = rcc_billing_conn,
-  rc_conn = rc_conn
-)
-
-#standardize the datatypes
-service_request_line_items<-
-  dplyr::mutate_all(service_request_line_items, as.character)
-new_project_invoice_line_items<-
-  dplyr::mutate_all(new_project_invoice_line_items, as.character)
 
 # Row bind all new invoice line items and add IDs
 new_invoice_line_item_writes <- dplyr::bind_rows(
@@ -80,7 +96,9 @@ new_invoice_line_item_writes <- dplyr::bind_rows(
   dplyr::mutate(
     id = row_number() + max(initial_invoice_line_item$id),
     .before = "service_identifier"
-  )
+  ) |>
+  # TODO: remove this line after we add support for fiscal contact info in https://github.com/ctsit/rcc.billing/milestone/21
+  select(-starts_with("fiscal_contact"))
 
 # Write the new invoice line items
 redcapcustodian::write_to_sql_db(
@@ -130,9 +148,9 @@ new_invoice_line_item_communications <- draft_communication_record_from_line_ite
   mutate(id = row_number() + max_id_in_invoice_line_item_communications)
 
 # Email CSBT
-email_subject <- paste("New invoice line items for REDCap Annual Project Maintenance")
+email_subject <- paste("New invoice line items for REDCap Automated Billing")
 attachment_object <- sendmailR::mime_part(tmp_invoice_file, new_invoice_line_items_filename)
-body <- "The attached file has new invoice line items for REDCap Annual Project Maintenance. Please load these into the CSBT invoicing system."
+body <- "The attached file has new invoice line items for REDCap Automated Billing. Please load these into the CSBT invoicing system."
 email_body <- list(body, attachment_object)
 send_email(
   email_body = email_body,
