ba1f077b133bdd1ce039b32a61712f4b5b757b35
Philip Chase <pbc@ufl.edu>
2025-05-20 12:14:14 -0400

    Update sync_redcap_projects_to_history.R
    Create history table if it does not exist.
    Ensure the source table does not add columns.
    Test if there is data to sync.
    Sync and log only if there is data to sync.

--- etl/sync_redcap_projects_to_history.R
+++ etl/sync_redcap_projects_to_history.R
@@ -11,52 +11,50 @@ init_etl("sync_redcap_projects_to_history")
 rc_conn <- connect_to_redcap_db()
 rc_billing_conn <- connect_to_rcc_billing_db()
 
-redcap_projects_source <- tbl(rc_conn, "redcap_projects") |> collect()
-redcap_projects_history <- tbl(rc_billing_conn, "redcap_projects_history") |> collect()
-
-# find all <datetime<UTC>>
-dt_columns <- redcap_projects_source |>
-  dplyr::select(where(~ lubridate::is.POSIXt(.x))) |>
-  colnames()
-
-convert_df_datetimes <- function(df, cols_to_convert) {
-  if (nrow(df) == 0) {
-    for (col_name in cols_to_convert) {
-      if (!col_name %in% names(df)) { df[[col_name]] <- as.POSIXct(character(0)) }
-      else if (!inherits(df[[col_name]], "POSIXct")) { df[[col_name]] <- as.POSIXct(character(0)) }
-    }
-    return(df)
-  }
-  df |>
-    dplyr::mutate(
-      dplyr::across(
-        tidyselect::any_of(cols_to_convert),
-        ~ lubridate::as_datetime(.x)
-      )
-    )
+# create the target table and load it with data if it does not exist
+if (!"redcap_projects_history" %in% DBI::dbListTables(rc_billing_conn)) {
+  redcap_projects_source <- tbl(rc_conn, "redcap_projects") |> collect()
+  result <- DBI::dbWriteTable(rc_billing_conn, "redcap_projects_history", redcap_projects_source)
 }
 
-redcap_projects_source_typed <- convert_df_datetimes(redcap_projects_source, dt_columns)
-redcap_projects_history_typed <- convert_df_datetimes(redcap_projects_history, dt_columns)
+redcap_projects_history <- tbl(rc_billing_conn, "redcap_projects_history") |> collect()
 
-sync_activity_results <- redcapcustodian::sync_table_2(
-  conn = rc_billing_conn,
-  table_name = "redcap_projects_history",
-  source = redcap_projects_source_typed,
-  source_pk = "project_id",
-  target = redcap_projects_history_typed,
-  target_pk = "project_id",
-  insert = T,
-  update = T,
-  delete = F
+# Ensure redcap_projects_source does not have any columns not already in redcap_projects_history
+# This protects us from the more common alterations to redcap_projects
+redcap_projects_source <- tbl(rc_conn, "redcap_projects") |>
+  collect() |>
+  select(any_of(names(redcap_projects_history)))
+
+# If there is novel data in the source, sync the data
+novel_data <- all.equal(
+  redcap_projects_source,
+  redcap_projects_history |>
+    filter(project_id %in% redcap_projects_source$project_id)
 )
+novel_data <- if_else(length(novel_data) > 1, T, F)
 
-summary_data <- list(
-  updates = sync_activity_results$update_n,
-  inserts = sync_activity_results$insert_n,
-  records_updated = sync_activity_results$update_records,
-  records_inserted = sync_activity_results$insert_records,
-  records_deleted = sync_activity_results$delete_records
-)
+if (novel_data) {
+  # sync the data
+  sync_activity_results <- redcapcustodian::sync_table_2(
+    conn = rc_billing_conn,
+    table_name = "redcap_projects_history",
+    source = redcap_projects_source,
+    source_pk = "project_id",
+    target = redcap_projects_history,
+    target_pk = "project_id",
+    insert = T,
+    update = T,
+    delete = F
+  )
 
-log_job_success(jsonlite::toJSON(summary_data))
+  # log the work
+  summary_data <- list(
+    updates = sync_activity_results$update_n,
+    inserts = sync_activity_results$insert_n,
+    records_updated = sync_activity_results$update_records,
+    records_inserted = sync_activity_results$insert_records,
+    records_deleted = sync_activity_results$delete_records
+  )
+
+  log_job_success(jsonlite::toJSON(summary_data))
+}
