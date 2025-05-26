cc4fb3dc3eb6c209107173240c6b8981258fbad1
Sai Pavan Kamma <32958525+saipavan10-git@users.noreply.github.com>
2025-05-26 19:27:43 -0400

    Refactor etl backfill_redcap_history_table
    - Use the replay SQL log approach

--- etl/single-use/backfill_redcap_history_table.R
+++ etl/single-use/backfill_redcap_history_table.R
@@ -7,99 +7,72 @@ library(dotenv)
 library(jsonlite)
 library(lubridate)
 
-init_etl("backfill_redcap_history_table")
+init_etl("backfill_redcap_project_history_table_from_log_event_table")
 
 rc_conn <- connect_to_redcap_db()
 rc_billing_conn <- connect_to_rcc_billing_db()
 
-NA_POSIXct_ <- as.POSIXct(NA)
-
-creation_data_from_redcap_projects <- dplyr::tbl(rc_conn, "redcap_projects") |>
-  dplyr::select(project_id, creation_time) |>
-  dplyr::collect() |>
-  dplyr::mutate(creation_time = lubridate::as_datetime(creation_time))
-
-redcap_projects_history_current <- dplyr::tbl(rc_billing_conn, "redcap_projects_history") |>
-  dplyr::collect()
-
-convert_df_datetimes <- function(df, cols_to_convert, tz = "UTC") {
-  if (nrow(df) == 0) {
-    for (col_name in cols_to_convert) {
-      if (!col_name %in% names(df)) {
-        df[[col_name]] <- as.POSIXct(character(0), tz = tz)
-      } else if (!inherits(df[[col_name]], "POSIXct")) {
-        df[[col_name]] <- as.POSIXct(df[[col_name]], tz = tz)
-      }
-    }
-    return(df)
-  }
-  df |>
-    dplyr::mutate(
-      dplyr::across(
-        tidyselect::any_of(cols_to_convert),
-        ~ lubridate::as_datetime(.x, tz = tz)
-      )
-    )
+# create a target table if it does not exist. empty after creation.
+if (!"redcap_projects_history" %in% DBI::dbListTables(rc_billing_conn)) {
+  redcap_projects_source <- tbl(rc_conn, "redcap_projects") |> collect()
+  result <- DBI::dbWriteTable(rc_billing_conn, "redcap_projects_history", redcap_projects_source)
+  DBI::dbExecute(rc_billing_conn, "truncate redcap_projects_history")
 }
 
-cols_to_convert <- creation_data_from_redcap_projects |>
-  dplyr::select(where(~ lubridate::is.POSIXt(.x))) |>
-  colnames()
+system.time({
+  project_life_cycle <- get_project_life_cycle(rc_conn)
+})
 
-creation_data_from_redcap_projects <- creation_data_from_redcap_projects |>
-  convert_df_datetimes(cols_to_convert)
+sql_statements_to_replay <-
+  project_life_cycle |>
+  filter(str_detect(sql_log, "^update redcap_projects")) |>
+  filter(!str_detect(sql_log, "^delete")) |>
+  filter(!str_detect(description, "report")) |>
+  filter(project_id != 0) |>
+  select(log_event_id, project_id, description, sql_log) |>
+  mutate(sql_log = str_replace(sql_log, "^update redcap_projects ", "update redcap_projects_history "))
 
-redcap_projects_history <- redcap_projects_history_current |>
-  convert_df_datetimes(cols_to_convert)
+max_project_id <- sql_statements_to_replay |>
+  summarize(max_project_id = max(project_id)) |>
+  pull(max_project_id)
 
-project_life_cycle <- get_project_life_cycle(rc_conn)
-
-creation_and_deletion_facts <- project_life_cycle |>
-  select(project_id, ts, description) |>
-  group_by(project_id) |>
-  mutate(field_name = case_when(
-    str_detect(description, "^Create project") ~ "creation_time",
-    str_detect(description, "^Copy project from") ~ "creation_time",
-    str_detect(description, "^Copy project$") ~ "creation_time",
-    str_detect(description, "^(D|d)elete project$") ~ "date_deleted"
-  )) |>
-  ungroup() |>
-  mutate(description = if_else(str_detect(description, "^delete project$"), "Delete project", description)) |>
-  arrange(desc(ts)) |>
-  distinct(project_id, description, .keep_all = TRUE) |>
-  filter(!is.na(field_name)) |>
-  mutate(value = ymd_hms(ts)) |>
-  select(-ts) |>
-  pivot_wider(
-    id_cols = "project_id",
-    names_from = "field_name",
-    values_from = "value"
-  ) |>
-  left_join(creation_data_from_redcap_projects, by = "project_id", suffix = c(".log", ".live")) |>
-  mutate(
-    creation_time = coalesce(creation_time.live, creation_time.log)
-  ) |>
-  arrange(project_id) |>
-  fill(creation_time, .direction = "downup") |>
-  select(project_id, creation_time, date_deleted)
-
-sync_summary <- redcapcustodian::sync_table_2(
-  conn = rc_billing_conn,
-  table_name = "redcap_projects_history",
-  source = creation_and_deletion_facts,
-  source_pk = "project_id",
-  target = redcap_projects_history,
-  target_pk = "project_id",
-  insert = TRUE,
-  update = TRUE,
-  delete = FALSE
+initial_projects <- tibble(
+  project_id = 1:max_project_id
 )
 
+result <- DBI::dbWriteTable(
+  rc_billing_conn,
+  "redcap_projects_history",
+  initial_projects,
+  append = TRUE
+)
+
+execution_results <- sql_statements_to_replay$sql_log |>
+  map_lgl(~ {
+    tryCatch(
+      {
+        DBI::dbExecute(rc_billing_conn, .x)
+        TRUE
+      },
+      error = function(e) {
+        FALSE
+      }
+    )
+  })
+
+successful_statements <- sum(execution_results)
+failed_statements <- sum(!execution_results)
+
+DBI::dbExecute(rc_billing_conn, "UPDATE redcap_projects_history h
+JOIN redcap_projects p USING(project_id)
+SET h.creation_time = p.creation_time
+WHERE h.creation_time IS NULL")
+
 activity_log <- list(
-  inserts = sync_summary$insert_n,
-  updates = sync_summary$update_n,
-  records_inserted = sync_summary$insert_records,
-  records_updated = sync_summary$update_records
+  statements_executed = successful_statements,
+  statements_failed = failed_statements,
+  total_statements = nrow(sql_statements_to_replay),
+  max_project_id = max_project_id
 )
 
 log_job_success(jsonlite::toJSON(activity_log, auto_unbox = TRUE, pretty = TRUE))
