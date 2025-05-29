eccfe0e4bbdc4bc1b286085f4f8100c457af4e4d
Philip Chase <pbc@ufl.edu>
2025-05-29 13:15:56 -0400

    Update backfill_redcap_history_table.R
    Create the redcap_projects_history table from 'SHOW CREATE TABLE' output.
    Measure run time of all slow steps
    Run first sync from redcap_projects.
    Interpolate missing creation dates.

--- etl/single-use/backfill_redcap_history_table.R
+++ etl/single-use/backfill_redcap_history_table.R
@@ -12,16 +12,23 @@ init_etl("backfill_redcap_project_history_table_from_log_event_table")
 rc_conn <- connect_to_redcap_db()
 rc_billing_conn <- connect_to_rcc_billing_db()
 
-# create a target table if it does not exist. empty after creation.
+# create a target table if it does not exist.
 if (!"redcap_projects_history" %in% DBI::dbListTables(rc_billing_conn)) {
-  redcap_projects_source <- tbl(rc_conn, "redcap_projects") |> collect()
-  result <- DBI::dbWriteTable(rc_billing_conn, "redcap_projects_history", redcap_projects_source)
-  DBI::dbExecute(rc_billing_conn, "truncate redcap_projects_history")
+  create_redcap_projects_history <-
+    DBI::dbGetQuery(rc_conn, "SHOW CREATE TABLE redcap_projects") |>
+    janitor::clean_names() |>
+    # rename the table
+    mutate(create_table = str_replace(create_table, "`redcap_projects`", "`redcap_projects_history`")) |>
+    # remove all constraints because we are not creating any of the referenced tables
+    mutate(create_table = str_replace_all(create_table, ",\n  CONSTRAINT [^,]+ CASCADE", "")) |>
+    pull(create_table)
+  DBI::dbExecute(rc_billing_conn, create_redcap_projects_history)
 }
 
-system.time({
+elapsed_run_time_of_get_project_life_cycle <- system.time({
   project_life_cycle <- get_project_life_cycle(rc_conn)
 })
+elapsed_run_time_of_get_project_life_cycle
 
 sql_statements_to_replay <-
   project_life_cycle |>
@@ -47,26 +54,93 @@ result <- DBI::dbWriteTable(
   append = TRUE
 )
 
-execution_results <- sql_statements_to_replay$sql_log |>
-  map_lgl(~ {
-    tryCatch(
-      {
-        DBI::dbExecute(rc_billing_conn, .x)
-        TRUE
-      },
-      error = function(e) {
-        FALSE
-      }
-    )
-  })
+elapsed_run_time_of_replay <- system.time({
+  execution_results <- sql_statements_to_replay$sql_log |>
+    map_lgl(~ {
+      tryCatch(
+        {
+          DBI::dbExecute(rc_billing_conn, .x)
+          TRUE
+        },
+        error = function(e) {
+          FALSE
+        }
+      )
+    })
+})
+elapsed_run_time_of_replay
 
 successful_statements <- sum(execution_results)
 failed_statements <- sum(!execution_results)
 
-DBI::dbExecute(rc_billing_conn, "UPDATE redcap_projects_history h
-JOIN redcap_projects p USING(project_id)
-SET h.creation_time = p.creation_time
-WHERE h.creation_time IS NULL")
+# Sync the current redcap_projects table to the new history table
+redcap_projects_history <- tbl(rc_billing_conn, "redcap_projects_history") |> collect()
+
+# Ensure redcap_projects_source does not have any columns not already in redcap_projects_history
+# This protects us from the more common alterations to redcap_projects
+redcap_projects_source <- tbl(rc_conn, "redcap_projects") |>
+  collect() |>
+  select(any_of(names(redcap_projects_history)))
+
+# sync the data
+elapsed_run_time_of_rp_sync <- system.time({
+  sync_activity_results <- redcapcustodian::sync_table_2(
+    conn = rc_billing_conn,
+    table_name = "redcap_projects_history",
+    source = redcap_projects_source,
+    source_pk = "project_id",
+    target = redcap_projects_history,
+    target_pk = "project_id",
+    insert = T,
+    update = T,
+    delete = F
+  )
+})
+elapsed_run_time_of_rp_sync
+
+# log the work
+rp_summary_data <- list(
+  rp_updates = sync_activity_results$update_n,
+  rp_inserts = sync_activity_results$insert_n,
+  rp_records_updated = sync_activity_results$update_records,
+  rp_records_inserted = sync_activity_results$insert_records,
+  rp_records_deleted = sync_activity_results$delete_records
+)
+
+# fill the blank creation dates via interpolation
+redcap_projects_history <- tbl(rc_billing_conn, "redcap_projects_history") |> collect()
+
+first_record_with_non_na_creation_time <-
+  redcap_projects_history |>
+    filter(!is.na(creation_time)) |>
+    filter(project_id == min(project_id)) |>
+    pull(project_id)
+
+interpolated_creation_dates <- redcap_projects_history |>
+  filter(project_id >= first_record_with_non_na_creation_time) |>
+  mutate(creation_time = as.POSIXct(zoo::na.approx(creation_time), tz="UTC")) |>
+  select("project_id", "creation_time")
+
+elapsed_run_time_of_ct_sync <- system.time({
+  creation_time_sync_results <- redcapcustodian::sync_table_2(
+    conn = rc_billing_conn,
+    table_name = "redcap_projects_history",
+    source = interpolated_creation_dates,
+    source_pk = "project_id",
+    target = redcap_projects_history |> select("project_id", "creation_time"),
+    target_pk = "project_id",
+    insert = F,
+    update = T,
+    delete = F
+  )
+})
+elapsed_run_time_of_ct_sync
+
+ct_summary_data <- list(
+  ct_updates = creation_time_sync_results$update_n,
+  ct_inserts = creation_time_sync_results$insert_n,
+  ct_records_updated = creation_time_sync_results$update_records
+)
 
 activity_log <- list(
   statements_executed = successful_statements,
@@ -76,3 +150,6 @@ activity_log <- list(
 )
 
 log_job_success(jsonlite::toJSON(activity_log, auto_unbox = TRUE, pretty = TRUE))
+# rp_summary is too long to write, so skip it
+# log_job_success(jsonlite::toJSON(rp_summary_data, auto_unbox = TRUE, pretty = TRUE))
+log_job_success(jsonlite::toJSON(ct_summary_data, auto_unbox = TRUE, pretty = TRUE))
